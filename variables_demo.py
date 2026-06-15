0d28c2c243706e92a83cb9ec75657394f7c2179d
Qi686 <a91174@outlook.com>
2026-06-15 16:44:54 +0800

    feat:添加变量和简单数据类型演示脚本

---
+++ variables_demo.py
@@ -0,0 +1,63 @@
+name= "启文"
+message= "你好，" + name
+print(message)
+
+first_name = "qi"
+last_name = "wen"
+full_name = first_name + "" + last_name
+
+message = first_name.title() + last_name.upper() + "！"
+print(message)  
+
+print("\tPython\n2\t3\n4")
+
+Kong_Bai = " 空白 "
+Kong_Bai = Kong_Bai.lstrip()
+Kong_Bai = Kong_Bai.rstrip()
+Kong_Bai = Kong_Bai.strip()
+print(Kong_Bai) 
+
+message = "one of Python's strengths is its diverse and supportive community."
+print(message)
+
+# 练习
+
+# 个性化消息
+name_1 = "eric"
+name_1=name_1.title()
+message="Hello"+" "+name_1+",would you like to learn some 'python' today"
+print(message)  
+
+# 调整名字的大小写
+name_2="brien"
+name_2=name_2.title()
+print(name_2)
+name_2=name_2.upper()
+print(name_2)
+name_2=name_2.lower()
+print(name_2)
+
+# 打印名言
+message='Albert Einstein once said,"A person who never made a mistake never tried anything new."'
+print(message) 
+
+# 名言2
+famous_person="Albert Einstein"
+message=famous_person+" "+'once said,'+'"A person who never made a mistake never tried anything new."'
+print(message)
+
+# 剔除人名中的空白
+name_3=' "bluen" '
+print("\t\nbb\tblue")
+name_3=name_3.lstrip()
+print(name_3)
+name_3=name_3.rstrip()
+print(name_3)
+name_3=name_3.strip()
+print(name_3)
+
+age=24
+message="Happy "+str(age)+"rd Birthday!"
+print(message) 
+
+print(3.0/3.0)
